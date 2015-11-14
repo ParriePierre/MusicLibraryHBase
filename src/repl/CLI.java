ed3059934de2d61333f8e68ca29d6b6e343ecdd3
ParriePierre <pierre.parrie@gmail.com>
2015-11-14 16:23:39 +0100

    Clearly define CLI

--- src/repl/CLI.java
+++ src/repl/CLI.java
@@ -5,117 +5,171 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class CLI {
-	
-	//Un objet contenant la requette put
+
+	// Un objet contenant la requette put
 	private Object request;
-	
+
 	private Printer view;
-	
+
 	private BufferedReader input;
-	
+
 	private int SpaceLocation;
-	private String col;
-	private String info;
-	
+	private int secondSpaceLocation;
+	private String columnDescriptor;
+	private String informationLabel;
+	private String informationValue;
+
 	private int exitsignal;
-	
+
 	public CLI() {
 		view = new Printer();
 		input = new BufferedReader(new InputStreamReader(System.in));
 		SpaceLocation = -1;
-		col ="";
-		info ="";
+		secondSpaceLocation = -1;
+		columnDescriptor = "";
+		informationLabel = "";
+		informationValue = "";
 		exitsignal = 0;
 	}
+
+	/**
+	 * Handles a song request creation. 
+	 * @param input
+	 *            Contains the name of the song. The function handles the
+	 *            suppresion of the word "song "
+	 *            
+	 *      -> A COMPLETER
+	 */
+	private void createSong(String input) {
+		// remove "song " from the command
+		// Only the song name remains
+		input = input.substring(4);
+
+		/**
+		 * Create put request where song name is input
+		 */
+
+		view.NewSongRequest(input);
+	}
 	
-	public void eval(String input) {
-		//Determined the type of command
+	/**
+	 * Fill the information value of our table.
+	 * @param input Contains the ColumnDescriptor, information label and information to use to fill the put request. The function handles the removal of "information "
+	 * 
+	 *    -> A COMPLETER
+	 */
+	private void addInformationToSong(String input)
+	{
+		String tmp;
 		
-		if(input.contains("add "))
+		//remove "information " from the command
+		input = input.substring(11);
+		
+		//Obtain column name and information
+		SpaceLocation = input.indexOf(' ');
+		
+		if(SpaceLocation != -1)
 		{
-			//remove "add " from the command
-			input = input.substring(4);
+			columnDescriptor = input.substring(0, SpaceLocation);
 			
-			//Add a new song only if a song is not being registered
-			if(input.contains("song ") && request == null)
+			tmp = input.substring(SpaceLocation + 1);
+			secondSpaceLocation = tmp.indexOf(" ");
+			
+			if(SpaceLocation != -1)
 			{
-				//remove "song " from the command
-				//Only the song name remains
-				input = input.substring(4);
+				informationLabel = tmp.substring(0, secondSpaceLocation);
+				informationValue = tmp.substring(secondSpaceLocation + 1);
 				
 				/**
-				 * Create put request where song name is input
+				 * Add to put request informations where col is the column and info the data
 				 */
 				
+				SpaceLocation = -1;
+				columnDescriptor = "";
+				informationLabel = "";
+				informationValue = "";
+				
 				view.NewSongRequest(input);
-				return;
-			} else if (input.contains("song ") && request != null) {
-				view.ExistingPutRequest();
-				return;
+			} else {
+				view.MissingArgument();
 			}
-			
-			//Add information to a song only if a song is being registered
-			if(input.contains("information ") && request != null)
-			{
-				//remove "information " from the command
-				input = input.substring(11);
-				
-				//Obtain column name and information
-				SpaceLocation = input.indexOf(' ');
-				
-				if(SpaceLocation != -1)
-				{
-					col = input.substring(0, SpaceLocation);
-					info = input.substring(SpaceLocation+1);
-				
-					/**
-					 * Add to put request informations where col is the column and info the data
-					 */
-					
-					SpaceLocation = -1;
-					col = "";
-					info = "";
-					
-					view.NewSongRequest(input);
-					return;
-				} else {
-					view.MissingArgument();
-					return;
-				}
-			} else if (input.contains("song ") && request == null) {
-				view.NoExistingSong();
-				return;
-			}
-			
-		} 
+		} else {
+			view.MissingArgument();
+		}
+	}
+	
+	/**
+	 * Handles all the treatment if the input has "add" word.
+	 * @param input Command entered by the user
+	 */
+	private void addToPull(String input)
+	{
+		//remove "add " from the command
+		input = input.substring(4);
 		
-		//Commit the request nly if it has been instanciated
-		/**
-		 * add a control if informations are empty 
-		 */
-		if(input.contains("commit") && request != null)
+		//Add a new song only if a song is not being registered
+		if(input.contains("song ") && request == null)
 		{
-			/**
-			 * Commit the put request
-			 */
-			
-			view.CommitedRequest();
+			createSong(input);
+		} else if (input.contains("song ") && request != null) {
+			view.ExistingPutRequest();
+		}
+		
+		//Add information to a song only if a song is being registered
+		if(input.contains("information ") && request != null)
+		{
+			addInformationToSong(input);
+		} else if (input.contains("information ") && request == null) {
+			view.NoExistingSong();
+		}
+	}
+	
+	/**
+	 * Request the put object to commit its values
+	 * 
+	 * 		-> A COMPLETER
+	 */
+	private void performCommit()
+	{
+		/**
+		 * Commit the put request
+		 */
+
+		view.CommitedRequest();
+	}
+	
+	/**
+	 * Evaluate the first word of the input and acts accordingly
+	 * @param input the input.
+	 */
+	private void eval(String input) {
+		// Determined the type of command
+
+		if (input.contains("add ")) {
+			addToPull(input);
 			return;
+		}
+
+		// Commit the request only if it has been instanciated
+		/**
+		 * add a control if informations are empty
+		 */
+		if (input.contains("commit") && request != null) {
+			performCommit();
 		} else if (input.contains("commit") && request == null) {
 			view.NoExistingSong();
 			return;
 		}
-		
-		if(input.contains("exit"))
-		{
+
+		if (input.contains("exit")) {
 			exitsignal = 1;
 			view.ExitMessage();
 			return;
 		}
-		
+
 		view.NotReconizedCommand();
 	}
-	
+
 	public void loop() {
 		do {
 			try {
--- src/repl/Printer.java
+++ src/repl/Printer.java
@@ -6,8 +6,28 @@ public class Printer {
 		System.out.println("Commands : ");
 		System.out.println("add song <<song name>> : Create a song reqest");
 		System.out.println("	Only one song request may be instanciated at a time. You need to commit the request before creating a new one.");
-		System.out.println("add information <<information label>> <<information>> : add information to a song");
+		System.out.println("add information <<column Descriptor>> <<information label>> <<information>> : add information to a song");
 		System.out.println("	You can only add information once a song request has been created.");
+		System.out.println("commit : Execute a put request in HBase.");
+		System.out.println("exit : quit MusicLibrary project.");
+		System.out.println("The database to fill follows this pattern : ");
+		System.out.println("Key : song name");
+		System.out.println("Column descriptors :");
+		System.out.println("\t - Song : information labels : ");
+		System.out.println("\t\t - Name");
+		System.out.println("\t\t - MusicStyle");
+		System.out.println("\t\t - Duration");
+		System.out.println("\t\t - PathToMP3");
+		System.out.println("\t\t - PathToCover");
+		System.out.println("\t\t - Score");
+		System.out.println("\t\t - ReleaseDate");
+		System.out.println("\t - Artist : information labels : ");
+		System.out.println("\t\t - Name");
+		System.out.println("\t\t - Description");
+		System.out.println("\t - Album : information labels : ");
+		System.out.println("\t\t - Name");
+		System.out.println("\t\t - ReleaseDate");
+		System.out.println("\t\t - TrackNumber");
 		System.out.print(">");
 	}
 	
@@ -37,22 +57,22 @@ public class Printer {
 	 * ERROR MESSAGES *
 	 *****************/
 	protected void ExistingPutRequest() {
-		System.out.println("A put request is already running.");
+		System.err.println("A put request is already running.");
 		welcomeMessage();
 	}
 	
 	protected void NoExistingSong() {
-		System.out.println("No request for a song has been created.");
+		System.err.println("No request for a song has been created.");
 		welcomeMessage();
 	}
 	
 	protected void MissingArgument() {
-		System.out.println("Missing argument.");
+		System.err.println("Missing argument.");
 		welcomeMessage();
 	}
 	
 	protected void NotReconizedCommand() {
-		System.out.println("Unknown command.");
+		System.err.println("Unknown command.");
 		welcomeMessage();
 	}
 }
